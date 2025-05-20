ff91dde907343e2c011d59e73de6bd137780e1b4
Sagar Taunk <sagartaunk2@gmail.com>
2025-05-20 21:55:08 +0530

    Finalised Client

--- Client/client.py
+++ Client/client.py
@@ -2,114 +2,137 @@ import requests
 import json
 import getpass
 
-#GENERIC FUNCTIONS
-def generic1(url) : #Sends The Server Email And Password
+# GENERIC FUNCTIONS
+def generic1(url):
     print("Please Enter The Credentials")
     email = input("[~] Username: ")
     password = getpass.getpass("[~] Password: ")
-    data = {"email":email , "password":password}
-    response = requests.post(url ,json=data)
+    data = {"email": email, "password": password}
+    response = requests.post(url, json=data)
     status = error_handelling(response.status_code)
-    if status :
-        return [email , password , status , response.json]
-    else :
-        generic1(url)
-    
+    if status:
+        return [email, password, status, response.json()]
+    else:
+        return generic1(url)
 
-     
-def error_handelling(status) : #Handels The Errors
-    match status :
-        case 200 :
-            return True 
-        case _ :
+def error_handelling(status):
+    match status:
+        case 200:
+            return True
+        case _:
             print("[~] ATTEMPT FAILED!!")
             choice = input("[~] Try Again [y,n] : ")
-            if choice == 'y' :
+            if choice == 'y':
                 return False
-            elif choice == 'n' :
+            elif choice == 'n':
                 return True
-            else :
+            else:
                 print("Invalid Choice")
-                error_handelling(status)
+                return error_handelling(status)
 
+# COMMUNICATION FUNCTIONS
 
-
-#COMMUNICATION FUNCTIONS
-
-def login(base_url): #Contacts Login endpoint
+def login(base_url):
     url = f"{base_url}/login"
     response = generic1(url)
-    if response[2] :
-        print("Successfull")
+    if response[2]:
+        print("[~] Successful")
         return response
-    else :
-        print("Failed to Login")
+    else:
+        print("[~] Failed to Login")
 
-
-def register(base_url): #Contacts Register endpoint
+def register(base_url):
     url = f"{base_url}/register"
     response = generic1(url)
-    if response[2] :
-        print("[~] Account Created Successfully Please Login using the details ")
-    else :
-        print("[~] Failed to Create Account ")
+    if response[2]:
+        print("[~] Account Created Successfully. Please Login using the details.")
+    else:
+        print("[~] Failed to Create Account")
 
-def get_data(url , data) : #Used to fetch usernames and websites of the stored accounts
-    url = f"{url}/get_data"
-    response = generic1(url , json=data)
-    if response[2] :
+def get_data(base_url, email, password):
+    url = f"{base_url}/get_data"
+    data = {"email": email, "password": password}
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
         print("[~] Successful")
-        print(response[3])
-        return response[3]
-    else :
-        print("[~] Failed ")
-
-def add_data(url , data) : #Used to add data to the master account 
-    url = f"{url}/add_data"
-    email = input("[~] Email / Username : ")
-    website = input("[~] Website : ")
-    password = getpass.getpass("[~] Password : ")
-    data.append({"data" : f"{email}:{website}:{password}"})
-    response = requests.post(url ,json=data)
-    if response.status_code == 200 :
-        print("[~] Success")
-    else :
+        print(response.json())
+        return response.json()
+    else:
         print("[~] Failed")
 
-def decrypt(url , data) :
-    data = get_data(url , data)
+def add_data(base_url, email, password):
+    url = f"{base_url}/add_data"
+    username = input("[~] Email / Username : ")
+    website = input("[~] Website : ")
+    passwd = getpass.getpass("[~] Password : ")
+    data = {
+        "email": email,
+        "password": password,
+        "data": f"{username}:{website}:{passwd}"
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print("[~] Success")
+    else:
+        print("[~] Failed")
 
+def decrypt(base_url, email, password):
+    url = f"{base_url}/get_password"
+    username = input("[~] Username to decrypt: ")
+    website = input("[~] Website: ")
+    data = {
+        "email": email,
+        "master_password": password,
+        "username": username,
+        "website": website
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print("[~] Decrypted Data:")
+        print(response.json())
+    else:
+        print("[~] Failed to decrypt or not found.")
 
-
-#MAIN FUNCTION
+# MAIN FUNCTION
 print("Welcome to Password Manager")
 print("Type 'Help' for help")
 email = ""
 password = ""
-base_url = "http://192.168.0.100:51020" #write the url of the middleware server 
-while True :
-    match input("[~] ") :
-        case "Login" :
-            result = login(base_url)
-            email = result[0]
-            password = result[1]
+base_url = "http://192.168.0.100:51020"  # Change to your middleware server
 
-        case "Register" :
-            result = register(base_url)
-        
-        case "Get" :
-            get_data(base_url , {"email" : email , "password" : password})
-        
-        case "Add" :
-            add_data(base_url , {"email" : email , "password" : password})
-        case "Decrypt" :
-            decrypt(base_url , {"email" : email , "password" : password})
-        case "Admin" :
-            print("Under Construction")
-        case "Help" :
+while True:
+    match input("[~] "):
+        case "Login":
+            result = login(base_url)
+            if result:
+                email = result[0]
+                password = result[1]
+        case "Register":
+            register(base_url)
+        case "Get":
+            if email and password:
+                get_data(base_url, email, password)
+            else:
+                print("[~] Please login first.")
+        case "Add":
+            if email and password:
+                add_data(base_url, email, password)
+            else:
+                print("[~] Please login first.")
+        case "Decrypt":
+            if email and password:
+                decrypt(base_url, email, password)
+            else:
+                print("[~] Please login first.")
+        case "Help":
             print("Here is a list of all commands you can perform : ")
-            print("Login : To login into a account \nRegister : To rgister a new account \nGet : To get the usernames and websites associated with the account \nDecrypt : Shows the list of associated usernames and websites and allows to decrypt the password of one account \nAdmin : To enter admin page \nExit : To Exit the program")
-        case "Exit" :
+            print("Login : To login into an account")
+            print("Register : To register a new account")
+            print("Get : To get the usernames and websites associated with the account")
+            print("Add : To add a new username/website/password")
+            print("Decrypt : To decrypt the password for a username/website")
+            print("Exit : To Exit the program")
+        case "Exit":
             break
-        case "Admin" :
-            print("Under Construction")
+        case _:
+            print("[~] Unknown command. Type 'Help' for help.")
